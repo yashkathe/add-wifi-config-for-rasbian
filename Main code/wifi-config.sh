7fc726c0b5f539945963e29c81fda2b17056be8d
Yash Kathe <yashkathe123@gmail.com>
2021-05-04 17:03:39 +0530

    removed getopts

--- Main code/wifi-config.sh
+++ Main code/wifi-config.sh
@@ -1,104 +1,42 @@
-#!/bin/bash
+#!/bin/bash 
 
+#Run this function when wrong parameter is passed 
 
-#adding help to guide through the process 
-help() {
-	echo
-	echo '--------------------------------------------------------'
-	echo
-	echo 'this script adds wifi configuration to your raspberry pi '
-	echo 
-	echo '-h for ======> help'
-	echo '-a for ======> adding wifi configuration'
-	echo
-	echo '--------------------------------------------------------'
-	echo
+non_zero_exit() {
+    echo
+    echo "The paramter you entered is wrong" 
+    echo "use --add while calling the script to add your wifi config"
+    echo 'or'
+    echo "use --help "
+    echo
+}
+
+#Run this function when user wants to add wifi config
+
+add_wifi() {
+    read -p "Enter the name of your wifi : " WIFI_NAME
+
+    read -s -p "Enter the password of your wifi" WIFI_PASS
+
+    read -p "Enter the security mode of your wifi" WIFI_SEC
+}
+
+#Run this code when user asks for help
+
+help_func(){
+    echo 'help'
 }
 
 
-#else condition for get opts 
-elsey() {
-	echo 
-	echo "the command ${OPTION} you entered does not exist"
-	echo
-	echo 'type -h for help'
-	exit 1
-}
-
-
-#adding wifi config whilst taking input from user 
-getConfig() {
-read -p 'enter the name of your wifi : ' WIFINAME
-
-echo 
-
-echo "The name of your wifi is ${WIFINAME}"
-
-echo 
-
-read -s -p 'enter the password for ' WIFIPASS 
-
-echo
-
-echo "password added successfully"
-
-echo 
-
-read -p 'enter the type of security mode your wifi has ' WIFIMODE
-
-echo 
-
-echo "your wifi has ${WIFIMODE} mode of security"
-
-echo
-
-sleep 2s
-
-echo "adding details .. "
-
-sleep 3s
-
-echo 
-echo "reboot your machine to check if wifi is connected successfully"
-
-echo 'network={' 1>> write-file.txt
-
-echo "	ssid="${WIFINAME}"" 1>> write-file.txt
-echo " 	psk="${WIFIPASS}"" 1>> write-file.txt
-echo " 	key_mgnt=${WIFIMODE}" 1>> write-file.txt
-
-echo '}' 1>> write-file.txt
-
-}
-
-
-while getopts tha OPTION
-do
-	case "${OPTION}" in 
-		t)
-			echo 'testing'
-			;;
-		h)
-			HELP='true' 
-			;;
-		a)	
-			ADD='true'
-			;;
-		?)
-			elsey 
+case "${1}" in 
+    --help)
+        help_func
+        ;;
+    --add)
+        add_wifi
+        ;;
+    *)
+        non_zero_exit
+        exit 1
+        ;;
 esac
-done
-
-
-
-if [[ "${HELP}" = 'true' ]]
-then 
-	help
-	exit 0
-fi
-
-if [[ "${ADD}" = 'true' ]]
-then 
-	getConfig
-	exit 0
-fi 
